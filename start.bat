66da0f106bdc499d0524b0a070145e7d98327bc0
guanxx <guanxx0811@gamil.com>
2026-07-06 13:29:24 +0800

    Remove local scripts and cache files

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start /B python -m uvicorn backend.main:app --host 0.0.0.0 --port 8000
-timeout /t 3 /nobreak >nul
-start http://localhost:8000
-echo Press any key to stop...
-pause >nul
-taskkill /F /IM python.exe 2>nul
